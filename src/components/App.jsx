e152b892e85f8db08c05154d33612b6401165be9
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-01-08 11:09:24 +0530

    initialized git

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Hello</h1>
+      </header>
+      <main>Welcome</main>
+      <footer>Goodbye</footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import "./sass/main.scss";
+
+const root = ReactDOM.createRoot(document.body);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
