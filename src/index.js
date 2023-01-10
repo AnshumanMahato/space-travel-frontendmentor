e3e70191d44ae2d06e234a72689c5d5e0d052547
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-01-10 15:33:41 +0530

    pages initilized

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import App from "./App";
 import "./sass/main.scss";
 
 const root = ReactDOM.createRoot(document.body);
