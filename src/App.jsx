0ea0f045ae62dd47f32efb1b87d86d3bb0663c86
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-01-11 21:44:19 +0530

    homepage main section init

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
+import Home from "./pages/Home";
 
 const background = {
   backgroundImage: `url("/assets/home/background-home-desktop.jpg")`,
@@ -11,6 +12,7 @@ function App() {
   return (
     <div style={background}>
       <Header />
+      <Home />
     </div>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
-const Home = () => {};
+const Home = () => {
+  return (
+    <main>
+      <div className="hero-text">
+        <h5 className="heading-5">So, you want to travel to</h5>
+        <h1 className="heading-1">Space</h1>
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+    </main>
+  );
+};
 
 export default Home;
