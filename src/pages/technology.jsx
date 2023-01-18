d43c30f6485fa4c6c464a66b8c060c35d4b9a0d9
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-01-18 18:16:27 +0530

    tech page

--- src/pages/technology.jsx
+++ src/pages/technology.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Technology = () => {
-  return <div>Hello</div>;
+  return (
+    <main className="page">
+      <h5 className="page-heading heading-5">
+        <span>03</span>&nbsp;&nbsp;Space launch 101
+      </h5>
+    </main>
+  );
 };
 
 export default Technology;
